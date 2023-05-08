0defcf8006b06079de865dd8365f00a3323a0a82
Firgrep <awdeegrays@gmail.com>
2023-05-08 15:06:21 +0200

    added queue and stack

---
+++ src/DataStructures/Queue/Queue.js
@@ -0,0 +1,38 @@
+import DoublyLinkedList from "../DoublyLinkedList/DoublyLinkedList.js";
+
+class Queue {
+    constructor(maxSize = Infinity) {
+        this.queue = new DoublyLinkedList();
+        this.maxSize = maxSize;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    hasRoom() {
+        return this.size < this.maxSize;
+    }
+
+    enqueue(data) {
+        if (this.hasRoom()) {
+            this.queue.addToTail(data);
+            this.size++;
+        } else {
+            throw new Error("Queue is full!");
+        }
+    }
+
+    dequeue() {
+        if (!this.isEmpty()) {
+            const data = this.queue.removeHead();
+            this.size--;
+            return data;
+        } else {
+            throw new Error("Queue is empty!");
+        }
+    }
+}
+
+export default Queue;
---
+++ src/Tests/TestDoublyLinkedList/locationsNyc.js
@@ -0,0 +1,21 @@
+import DoublyLinkedList from "../../DataStructures/DoublyLinkedList/DoublyLinkedList.js";
+
+
+// === TESTING DOUBLY LINKED LIST ===
+const subway = new DoublyLinkedList();
+
+subway.addToHead('TimesSquare');
+subway.addToHead('GrandCentral');
+subway.addToHead('CentralPark');
+subway.printList();
+
+subway.addToTail('PennStation');
+subway.addToTail('WallStreet');
+subway.addToTail('BrooklynBridge');
+subway.printList();
+
+subway.removeHead();
+subway.removeTail();
+
+subway.removeByData('TimesSquare');
+subway.printList();
